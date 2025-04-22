c31f7c58b7b07189119be71a62f492d2c0b5e1c0
Andrei Shevyriaev <sheviryaev@gmail.com>
2025-04-22 22:43:27 +0300

    [c2][guess] Guess game functionality implementation

--- chapter_2/guess/src/main.rs
+++ chapter_2/guess/src/main.rs
@@ -1,3 +1,45 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn print_incorrect_guess() {
+    println!("==> Please input number between 1 and 100");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("==> Guess number between 1 and 100");
+    let secret_number: u32 = rand::rng().random_range(1..100);
+
+    loop {
+        println!("==> Input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(x) => {
+                if x < 1 || x > 100 {
+                    print_incorrect_guess();
+                    continue;
+                }
+                x
+            }
+            Err(_) => {
+                print_incorrect_guess();
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("==> {guess} is too small!"),
+            Ordering::Greater => println!("==> {guess} is too big!"),
+            _ => {
+                println!("[ You win!!! ]");
+                break;
+            }
+        }
+    }
 }
