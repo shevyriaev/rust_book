11d444ea12b9ea70092937c5095ca978b51ea4e9
Andrei Shevyriaev <sheviryaev@gmail.com>
2025-04-23 21:31:17 +0300

    [c3][fahrenheit] Fahrenheit conversion functionality implementation

--- chapter_3/fahrenheit/src/main.rs
+++ chapter_3/fahrenheit/src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Input degree in Fahrenheit");
+
+    loop {
+        let mut fd = String::new();
+
+        io::stdin()
+            .read_line(&mut fd)
+            .expect("Failed to read line");
+
+        let fd: f32 =
+            match fd.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Please input valid number");
+                    continue;
+                }
+            };
+
+        println!("{:.2}°F = {:.2}°C", fd, (fd - 32.0) * 5.0 / 9.0);
+        break;
+    }
 }
