d039bad7f91f3aaf437fd204836a2e7eb4563047
Andrei Shevyriaev <sheviryaev@gmail.com>
2025-04-23 21:39:00 +0300

    [c3][fibonacci] Add recursive version

--- chapter_3/fibonacci/src/main.rs
+++ chapter_3/fibonacci/src/main.rs
@@ -15,8 +15,16 @@ fn fibonacci(n: u32) -> u32 {
     }
 }
 
+fn rfibonacci(n: u32) -> u32 {
+    match n {
+        1 => 0,
+        2 => 1,
+        n => rfibonacci(n - 1) + rfibonacci(n - 2)
+    }
+}
+
 fn main() {
     for n in 1..20 + 1 {
-        println!("F({n}) = {}", fibonacci(n));
+        println!("F({n}) = {}\t Fr({n}) = {}", fibonacci(n), rfibonacci(n));
     }
 }
