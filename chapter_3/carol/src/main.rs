fefb1fa9ce1dcb7cf71da9d45b6c31c06fc631b7
Andrei Shevyriaev <sheviryaev@gmail.com>
2025-04-23 22:00:18 +0300

    [c3][carol] 12 Days of Xmas functionality implementation

--- chapter_3/carol/src/main.rs
+++ chapter_3/carol/src/main.rs
@@ -1,3 +1,59 @@
+const START:[&'static str; 2] = [
+    "On the",
+    "day of Christmas\n\
+    My true love sent to me"
+];
+
+const MIDDLE:[&'static str; 11] = [
+    "Twelve drummers drumming",
+    "Eleven pipers piping",
+    "Ten lords a-leaping",
+    "Nine ladies dancing",
+    "Eight maids a-milking",
+    "Seven swans a-swimming",
+    "Six geese a-laying",
+    "Five golden rings (five golden rings)",
+    "Four calling birds",
+    "Three French hens",
+    "Two turtle-doves"
+];
+
+const END:[&'static str; 3] = [
+    "A",
+    "And a",
+    "partridge in a pear tree",
+];
+
+const NUMERALS:[&'static str; 12] = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth"
+];
+
 fn main() {
-    println!("Hello, world!");
+    println!("\n{} {} {}", START[0], NUMERALS[0], START[1]);
+    println!("{} {}", END[0], END[2]);
+
+    for i in 1..12 {
+
+        println!("\n{} {} {}", START[0], NUMERALS[i], START[1]);
+
+        for j in &MIDDLE[(11-i)..] {
+            println!("{}", j);
+        }
+
+        println!("{} {}", END[1], END[2]);
+
+    }
+
+    println!("{} {}", END[1], END[2]);
 }
