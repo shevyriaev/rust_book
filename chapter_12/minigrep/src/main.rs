bd45c3c02d8952cbaa24d2bb24948eebc40b31e6
Andrei Shevyriaev <sheviryaev@gmail.com>
2025-04-27 17:06:53 +0300

    [c12][minigrep] Add 'Parameters' struct for parsing arguments

--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -1,16 +1,35 @@
-use std::{env, fs};
+use std::{env, fs, process};
+
+struct Parameters {
+    query: String,
+    file_path: String,
+}
+
+impl Parameters {
+    fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        Ok(Parameters {
+            query: args[1].clone(),
+            file_path: args[2].clone()
+        })
+    }
+}
 
 fn main() {
     let args:Vec<String> = env::args().collect();
+    let params = Parameters::build(&args)
+        .unwrap_or_else(|err|{
+            println!("Problem parsing arguments: '{err}'");
+            process::exit(1);
+        });
 
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("Search for '{}' in file '{}' with text:", params.query, params.file_path);
 
-    println!("Search for '{query}'");
-    println!("In file '{file_path}'");
-
-    let content = fs::read_to_string(file_path)
+    let content = fs::read_to_string(params.file_path)
         .expect("Should have been able to read the file");
 
-    println!("With text:\n{content}");
+    println!("{content}");
 }
