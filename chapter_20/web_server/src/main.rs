acdb6bdaf3829408edb6915f9501bc406295bca5
Andrei Shevyriaev <sheviryaev@gmail.com>
2025-05-12 12:15:01 +0300

    [c20][web_server] Print request data

--- chapter_20/web_server/src/main.rs
+++ chapter_20/web_server/src/main.rs
@@ -1,11 +1,22 @@
-use std::net::TcpListener;
+use std::io::{BufRead, BufReader};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(i: &usize, mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("===> [Request no.{}]", i + 1);
+    println!("{:#?}", http_request)
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    for stream in listener.incoming() {
-        let _stream = stream.unwrap();
-
-        println!("Connection established!");
+    for (i, stream) in listener.incoming().enumerate() {
+        handle_connection(&i, stream.unwrap());
     }
 }
